ac6debcba3454233f0569694d2216750452a1bf4
ustasnov <ustasnov@gmail.com>
2024-04-01 13:51:49 +0700

    Final changes

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,6 @@ import ChatModel from "./model/chatmodel";
 import ChatController from "./controller/chatcontroller";
 import LoginDialog from "./view/logindialog";
 
-//const serverUrl = "ws://localhost:7070";
 const serverUrl = "wss://ahj-ssewsb.onrender.com";
 const model = new ChatModel(serverUrl);
 const controller = new ChatController(model);
